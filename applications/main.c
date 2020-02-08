e40ceb44a7b054a349dd288cda9540968c9b406e
armink <armink.ztl@gmail.com>
2020-02-08 20:37:26 +0800

    【更新】studio 工程配置； 【增加】big blob 测试命令。
    Signed-off-by: armink <armink.ztl@gmail.com>

--- applications/main.c
+++ applications/main.c
@@ -29,3 +29,27 @@ void file_test(void)
 }
 MSH_CMD_EXPORT(file_test, file test)
 
+void big_env_blob_test()
+{
+#define ENV_SIZE  512
+
+    uint8_t value[ENV_SIZE];
+    uint8_t read_value[ENV_SIZE];
+    size_t return_len, saved_value_len;
+    EfErrCode result;
+
+
+    for (size_t i = 0; i < ENV_SIZE; i++)
+    {
+        value[i] = i % 256;
+    }
+
+    result =  ef_set_env_blob("big_env", value, ENV_SIZE);
+    rt_kprintf("ef_set_env_blob return: %d, size: %d\n", result, ENV_SIZE);
+
+    return_len = ef_get_env_blob("big_env", read_value, ENV_SIZE, &saved_value_len);
+    rt_kprintf("ef_get_env_blob return: %d, saved_value_len: %d\n", return_len, saved_value_len);
+
+    rt_kprintf("memcmp result: %d\n", memcmp(value, read_value, ENV_SIZE));
+}
+MSH_CMD_EXPORT(big_env_blob_test, big env blob test)
